de88a2de4eac3d8fbcdcd4cdce07b6fcbd028df0
pothedarsuhas <pothedarsuhas@gmail.com>
2019-09-15 13:31:14 +0530

    Update mongobackup.py
    with all db backup

--- mongobackup.py
+++ mongobackup.py
@@ -27,25 +27,38 @@ example
 > python mongobackup.py mongodb://<username>:<password>@<hostnameorip>:27017/?authSource=<dbname> /backups/mongo/
 '''
 
-from pymongo import MongoClient
-import sys
 import os
+import time
+import sys
 
-client = MongoClient(str(sys.argv[1]))
+# configs:
+interval_m = 1
+uri = sys.argv[1]
+outputs_dir = sys.argv[2]
 
-uri = str(sys.argv[1])
-outputDir = str(sys.argv[2])
 
 uri = uri.split(':')
 
 username = uri[1][2:]
 password = uri[2].split('@')[0]
-hostname = uri[2].split('@')[1]
+host = uri[2].split('@')[1]
 port =uri[-1].split('/')[0]
 
-databases = client.database_names()
+def render_output_locations():
+  return outputs_dir + time.strftime("%d-%m-%Y-%H:%M:%S")
 
-for database in databases:
-    os.system("mongodump --host " + hostname + " --port " + port + 
-    " --username " + username + " --password " + password + " --db " 
-    + database + " --out " + outputDir)
+def run_backup():
+    command = "mongodump"
+    command += " --host " + host
+    command += " --port " + port
+    command += " --username " + username
+    command += " --password " + password
+    command += " --out " + render_output_locations()
+    os.system(command)
+
+print("mongo backup process started")
+print("I will backup your mongo db every {0} minutes".format(interval_m))
+
+while True:
+  time.sleep(interval_m * 60)
+  run_backup()
